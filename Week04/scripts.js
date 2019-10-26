a794152a93cb5643e56aee0071acd9cf2b36770a
NicolaWinterson <nicola.l.winterson@gmail.com>
2019-10-26 18:54:03 +0200

    Updated version

--- Week04/scripts.js
+++ Week04/scripts.js
@@ -1,32 +1,109 @@
-/*document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.dropdown-trigger');
-    var instances = M.Dropdown.init(elems, options);
-  });
+/*DECLARING VARIABLES GLOBALLY
+var p = document.getElementById("otherLinks");
+var search = document.getElementById("searchBar");
+var c = document.getElementById("submenu-nav-li");
+var menuLinks = document.getElementById("menuLinks");
+*/
 
-  document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.collapsible');
-    var instances = M.Collapsible.init(elems, options);
-  });*/
-
-/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
 function toggleLinks() {
-    var myLinks = document.getElementById("myLinks");
+  console.log("linksChildren")
+  var menuLinks = document.getElementById("menuLinks");
+  
+  var p = document.getElementById("otherLinks");
+  var search = document.getElementById("searchBar");
+  var c = document.getElementById("submenu-nav-li");
+
+  p.style.display = "none"
+  search.style.display = "none"
+  c.style.display = "none"
+  
+  if (menuLinks.style.display === "block") {
+    menuLinks.style.display = "none";
+  } else {
+    menuLinks.style.display = "block";
+  }
+}
+
+document.getElementById("child").addEventListener("click", function (e) {
+  console.log("child clicked")
+  e = window.event || e;
+
+  if (this === e.target) {
+    console.log("event handled properly")
+    var c = document.getElementById("submenu-nav-li");
+    
     var p = document.getElementById("otherLinks");
+    var menuLinks = document.getElementById("menuLinks");
+    var search = document.getElementById("searchBar");
+    
+    menuLinks.style.display = "none"
     p.style.display = "none"
-    if (myLinks.style.display === "block") {
-      myLinks.style.display = "none";
+    search.style.display = "none"
+    
+    if (c.style.display === "block") {
+      c.style.display = "none";
     } else {
-      myLinks.style.display = "block";
+      c.style.display = "block";
     }
   }
+});
+
+
+document.getElementById("parent").addEventListener("click", function (e) {
+  console.log("event2 handled")
+  e = window.event || e;
+
+  if (this === e.target) {
+    console.log("menuLinks handled")
+    var menuLinks = document.getElementById("menuLinks");
+    var p = document.getElementById("otherLinks");
+    var search = document.getElementById("searchBar");
+    var c = document.getElementById("submenu-nav-li");
+
+    p.style.display = "none"
+    search.style.display = "none"
+    c.style.display = "none"
+
+    if (menuLinks.style.display === "block") {
+      menuLinks.style.display = "none";
+    } else {
+      menuLinks.style.display = "block";
+    }
+  }
+});
 
 function toggleOtherLinks() {
-    var otherLinks = document.getElementById("otherLinks");
-    var x = document.getElementById("myLinks");
-    x.style.display = "none"
-    if (otherLinks.style.display === "block") {
-      otherLinks.style.display = "none";
-    } else {
-      otherLinks.style.display = "block";
-    }
+  var p = document.getElementById("otherLinks");
+  
+  var search = document.getElementById("searchBar");
+  var menuLinks = document.getElementById("menuLinks");
+  var c = document.getElementById("submenu-nav-li");
+  
+  search.style.display = "none"
+  c.style.display = "none"
+  menuLinks.style.display = "none"
+  
+  if (p.style.display === "block") {
+    p.style.display = "none";
+  } else {
+    p.style.display = "block";
   }
+}
+
+function toggleSearch() {
+  var search = document.getElementById("searchBar");
+
+  var p = document.getElementById("otherLinks");
+  var menuLinks = document.getElementById("menuLinks");
+  var c = document.getElementById("submenu-nav-li");
+  
+  p.style.display = "none"
+  c.style.display = "none"
+  menuLinks.style.display = "none"
+  
+  if (search.style.display === "block") {
+    search.style.display = "none";
+  } else {
+    search.style.display = "block";
+  }
+}
