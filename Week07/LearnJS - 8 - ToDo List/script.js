4e3e7747994df612068ae96316f67110fc447ee4
NicolaWinterson <nicola.l.winterson@gmail.com>
2019-11-30 10:26:47 +0100

    Updated js file

--- Week07/LearnJS - 8 - ToDo List/script.js
+++ Week07/LearnJS - 8 - ToDo List/script.js
@@ -1,11 +1,40 @@
-let clickNum = 1
-const createLi = () => {
-  clickNum += 1;
-  let newLi = document.createElement("li")
-  let newLiText = document.createTextNode("Item No." + clickNum)
-  newLi.appendChild(newLiText)
-  document.getElementById("list1").appendChild(newLiText);
+const todoItems = [
+  'break things',
+  'fix them',
+]
+
+const renderList = () => {
+  clearList()
+  todoItems.forEach(appendItem)
 }
 
-const addNewItemButton = document.getElementById("btn4")
-addNewItemButton.addEventListener("click", createLi)
+const clearList = () => {
+  document
+    .querySelectorAll('#todos ul li')
+    .forEach(item => item.remove());
+}
+
+const appendItem = (item) => {
+  const newLi = document.createElement('li')
+  const newLiText = document.createTextNode(item)
+  newLi.appendChild(newLiText)
+  document.querySelector('#todos ul').appendChild(newLi)
+}
+
+const addItem = (element) => {
+  element.preventDefault()
+  element.stopPropagation()
+
+  const textInput = element.target[0]
+
+  todoItems.push(textInput.value)
+  renderList()
+  //reset value
+  textInput.value = ' '
+}
+
+document
+  .querySelector('#todos form')
+  .addEventListener('submit', addItem)
+
+renderList()
