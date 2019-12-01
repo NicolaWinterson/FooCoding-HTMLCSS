e0ecbd1cf05650f54772b9203ff3fe991c112883
NicolaWinterson <nicola.l.winterson@gmail.com>
2019-12-01 13:59:25 +0100

    Updated fies

---
+++ Week09/index.js
@@ -0,0 +1,54 @@
+console.log("hi")
+
+const clickNavBar = () => {
+    const allNavItems = document.querySelectorAll(".nav-item")
+    allNavItems.forEach(navItem => {
+        navItem.addEventListener("click", function (event) {
+            console.log(event.target.href)
+            event.preventDefault()
+            history.pushState(null, null, event.target.href)
+            renderContent()
+        })
+    })
+}
+
+const renderContent = () => {
+    const navLocation = document.location.pathname
+    const main = document.getElementById("dynamic")
+    main.innerHTML = ""
+    let content
+    let subtitle
+    switch (navLocation) {
+        case "/":
+            content = "Home"
+            subtitle = "you are home"
+            break
+        case "/blog":
+            content = "Blog"
+            break
+        case "/contact":
+            content = "Contact"
+            break
+        default:
+            break;
+    }
+    const newElement = document.createElement("h1")
+    const newPElement = document.createElement("p")
+    //let text = content
+    //let subText = subtitle
+    newElement.innerHTML = content
+    newPElement.innerHTML = subtitle
+    main.appendChild(newElement)
+    main.appendChild(newPElement)
+}
+
+//renderContent()
+clickNavBar()
+console.log(history.length)
+
+/*const homeLocation = document.location
+document.location = "http://localhost:1234/"
+const newElementHome = document.createElement("h1")
+newElementHome.innerHTML = "Home"
+homeLocation.appendChild(newElementHome)*/
+
