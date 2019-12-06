dc5c9f96df8fa44fe871487055ec0e9b22cfc959
NicolaWinterson <nicola.l.winterson@gmail.com>
2019-12-06 10:21:39 +0100

    Fixed broken JS file

--- Week08/LearnJS - 13 - Remove Items from an Array/script.js
+++ Week08/LearnJS - 13 - Remove Items from an Array/script.js
@@ -1,23 +1,3 @@
-<<<<<<< HEAD
-//step one, create an array
-let arrayItems = [
-    'first thing',
-    'second thing',
-    'third thing'
-]
-
-//step two, render array into HTML list
-const renderArray = (item) => {
-    const createLi = document.createElement('li')
-    const createLiText = document.createTextNode(item)
-    createLi.appendChild(createLiText)
-    document.getElementById('list1').appendChild(createLi)
-}
-arrayItems.forEach(item => renderArray(item))
-
-//step three, add event listener for when user clicks
-//step four, remove item from array
-=======
 //step one, create an array
 const todoItems = [
     'break things',
@@ -25,17 +5,22 @@ const todoItems = [
 ]
 console.log(todoItems.length)
 
-const appendItem = (item) => {
+const removeItem = (item, index) => {
+    todoItems.splice(index, 1)
+    renderList()
+}
+
+const appendItem = (item, index) => {
     const newLi = document.createElement('li')
     const newLiText = document.createTextNode(item)
     newLi.appendChild(newLiText)
     document.querySelector('#todos ul').appendChild(newLi)
-    //document.addEventListener("li")
+    newLi.addEventListener("click", removeItem)
 }
 
 const addItem = (element) => {
-    //element.preventDefault()
-    element.stopPropagation()
+    element.preventDefault()
+    //element.stopPropagation()
     const textInput = element.target[0]
     todoItems.push(textInput.value)
     const resetValue = () => {
@@ -56,33 +41,12 @@ const clearList = () => {
       .forEach(item => item.remove());
 }
 
-const removeListItems = (change) => {
-    change.addEventListener("click", removeItem)
-    console.log("you clicked!")
-}
-const removeItem = (index) => {
-    todoItems.splice(index, 1)
-    renderList()
-}
-//allListItems.forEach(removeListItems)
-
-/*
-const removeNode = (change) => {
-      change.addEventListener("click", removeItem)
-  
-const removeItem = (item, index) => {
-    //const index = todoItems.indexOf(item)
-    todoItems.splice(index, 1)
-}
-*/
-
 console.log(todoItems.indexOf())
-//allListItems.forEach(removeItem)
   
 document
     .querySelector('#todos form')
     .addEventListener('submit', addItem)
-    //.getElementById("list1")
   
 renderList()
->>>>>>> c74548612c04e618fe2382e86d7e162ba5600a39
+
+
