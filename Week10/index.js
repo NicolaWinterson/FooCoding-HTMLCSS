50ace1299fd72b68ed7adb74c2e8cc1767a17f5c
NicolaWinterson <nicola.l.winterson@gmail.com>
2020-01-16 10:33:06 +0100

    Updated files and change of background image

--- Week10/index.js
+++ Week10/index.js
@@ -1,1 +1,8 @@
 console.log("js files")
+
+document.getElementById('about-me').onclick = function() {
+    console.log("about me was clicked")
+    const toggleAboutInfo = document.getElementById("about-me-text");
+    toggleAboutInfo.style.display = toggleAboutInfo.style.display === 'none' ? 'block' : 'none';
+};
+
